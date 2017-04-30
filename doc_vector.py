8471930798327201fdf2e3453382245bfd21acab
parikshitd19 <parikshitd19@gmail.com>
2017-04-30 20:27:03 +0530

    doc_vector.py modified

--- doc_vector.py
+++ doc_vector.py
@@ -31,24 +31,41 @@ model = fasttext.load_model("wiki.en/wiki.en.bin")
 f = open("training_data.txt", "r")
 data = []
 label = []
+glob_count=0
 for line in f:
     l = line.strip('\n')
     l = l.split("__label__")
     data.append(l[0])
     label.append(l[1])
+    glob_count=glob_count+1
 
 count = 1
-
+fin=[]
+d=[]
 for l in data:
     h = l.split()
     create_index(count, h)
-    z = None
+    d.append(h)
+for l in d:
+    mean= None
     den = 0
-    for w in h:
-        c = math.log10(1 + index[count][w]) * \
-            math.log10(len(index.keys()) / idf[w])
-        # z=z+c*model[w]
-        den = den + c
-        # print(model[w])
-    # z=z/den
-    # print(z)
+    z=None
+    for w in l:
+        s=math.log10(glob_count/idf[w])
+        f=model[w]
+        z=z+(f-sum(f)/len(f))*s
+        den=den+s
+    z=z/den
+    fin.append(z)
+
+f=open("vector.txt","w")
+for y in fin:
+    f.write(y)
+f.close()
+f=open("label.txt","w")
+for l in label:
+    f.write(l)
+f.close()
+
+
+        
