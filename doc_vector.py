f36af8c433122dd9bbf7520d0d3367d88bad8171
naman <namanspace@gmail.com>
2017-04-30 20:02:01 +0530

    successful run of wiki model

--- doc_vector.py
+++ doc_vector.py
@@ -5,46 +5,50 @@ import math
 from collections import OrderedDict
 
 
-def create_index(c,l):
-	
-	for w in l:
-		if w not in index[c].keys():
-			index[c][w]=1
-		else:
-			index[c][w]=index[c][w]+1
-		
-	l=list(set(l))
-	for w in l:
-		if w not in idf.keys():
-			idf[w]=1
-		else:
-			idf[w]=idf[w]+1
-	return
+def create_index(c, l):
 
-index=defaultdict(dict)
-idf={}
+    for w in l:
+        if w not in index[c].keys():
+            index[c][w] = 1
+        else:
+            index[c][w] = index[c][w] + 1
 
-model=fasttext.load_model("model.bin")
+    l = list(set(l))
+    for w in l:
+        if w not in idf.keys():
+            idf[w] = 1
+        else:
+            idf[w] = idf[w] + 1
+    return
 
-f=open("preporocess_combined.txt","r")
-data=[]
-label=[]
+index = defaultdict(dict)
+idf = {}
+
+model = fasttext.load_model("wiki.en/wiki.en.bin")
+# print(model.model_name)
+# print(model.words)
+
+f = open("training_data.txt", "r")
+data = []
+label = []
 for line in f:
-	l=line.strip('\n')
-	l=l.split("__label__")
-	data.append(l[0])
-	label.append(l[1])
-count=1
-for l in data:
-	h=l.split()
-	create_index(count,h)
-	z=None
-	den=0
-	for w in h:
-		c=math.log10(1+index[count][w])*math.log10(len(index.keys())/idf[w])
-		#z=z+c*model[w]
-		den=den+c
-		print(model[w])
-	#z=z/den
-	#print(z)
+    l = line.strip('\n')
+    l = l.split("__label__")
+    data.append(l[0])
+    label.append(l[1])
 
+count = 1
+
+for l in data:
+    h = l.split()
+    create_index(count, h)
+    z = None
+    den = 0
+    for w in h:
+        c = math.log10(1 + index[count][w]) * \
+            math.log10(len(index.keys()) / idf[w])
+        # z=z+c*model[w]
+        den = den + c
+        # print(model[w])
+    # z=z/den
+    # print(z)
--- generate_word_vectors.py
+++ generate_word_vectors.py
@@ -1,11 +1,11 @@
 import fasttext
 
-model=fasttext.skipgram("withoutlabels_combined.txt","model")
+# model = fasttext.skipgram("withoutlabels_combined.txt", "model")
 
 
 #model = fasttext.load_model("wiki.en/wiki.en.bin")
-classifier = fasttext.supervised(
-    "training_data.txt", "model", label_prefix="_label_")
-result = classifier.test("training_data.txt")
-print(classifier.labels)
-print(result.precision)
+# classifier = fasttext.supervised(
+# "training_data.txt", "model", label_prefix="_label_")
+# result = classifier.test("training_data.txt")
+# print(classifier.labels)
+# print(result.precision)
